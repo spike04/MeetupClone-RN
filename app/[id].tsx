cdcddaf44f8ded3eb6942dd282d70a9b2eeead4e
spike04 <rubinbajracharya@yahoo.com>
2024-09-10 11:47:51 +0545

    wip: event detail ui design

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,3 +1,4 @@
+import { Feather } from '@expo/vector-icons';
 import dayjs from 'dayjs';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
@@ -17,7 +18,12 @@ const EventDetails = () => {
   return (
     <>
       <Stack.Screen
-        options={{ title: event?.title, headerBackTitleVisible: false, headerTintColor: '#333' }}
+        options={{
+          title: event?.title,
+          headerBackTitleVisible: false,
+          headerTintColor: '#333',
+          headerRight: () => <Feather name="bookmark" size={20} color="gray" />,
+        }}
       />
       <View className="flex-1 gap-3 bg-white p-4">
         <Image className="aspect-video w-full rounded-xl" source={{ uri: event.image }} />
@@ -30,6 +36,42 @@ const EventDetails = () => {
         <Text className="text-md" numberOfLines={2}>
           {event.description}
         </Text>
+
+        <TouchableOpacity className="mt-8 flex-row items-start gap-4">
+          <Feather name="calendar" size={20} />
+          <View className="flex-1 flex-row items-center justify-between">
+            <View className="gap-1">
+              <Text className="text-base font-semibold">
+                {dayjs(event.datetime).format('dddd, D MMMM YYYY')}
+              </Text>
+              <View className="flex-row items-center gap-2">
+                <Text className="text-sm font-light">
+                  {dayjs(event.datetime).format('h:mm')} - {dayjs(event.datetime).format('h:mm')}{' '}
+                  CEST
+                </Text>
+                <Feather name="info" size={14} color="gray" />
+              </View>
+            </View>
+            <Feather name="chevron-right" size={24} color="gray" className="ml-auto" />
+          </View>
+        </TouchableOpacity>
+
+        <View className="h-px bg-gray-100" />
+
+        <TouchableOpacity className="flex-row items-start gap-4">
+          <Feather name="map-pin" size={20} />
+          <View className="flex-1 flex-row items-center justify-between">
+            <View className="gap-1">
+              <Text className="text-base font-semibold">{event.location}</Text>
+              <View className="flex-row items-center gap-2">
+                <Text className="text-sm font-light">{event.location}</Text>
+              </View>
+            </View>
+            <Feather name="chevron-right" size={24} color="gray" className="ml-auto" />
+          </View>
+        </TouchableOpacity>
+
+        <View className="h-px bg-gray-100" />
 
         {/* Footer */}
         <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t border-gray-200 bg-white p-4 pb-10">
