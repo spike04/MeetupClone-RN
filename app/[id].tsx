c47109be910e40a5b58156117e205bca538dc000
spike04 <rubinbajracharya@yahoo.com>
2024-09-10 09:50:22 +0545

    wip: event details page data

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,7 @@
-import { useLocalSearchParams } from 'expo-router';
+import dayjs from 'dayjs';
+import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import events from '@/assets/events.json';
 
@@ -9,10 +10,28 @@ const EventDetails = () => {
 
   const event = events.find((e) => e.id === id);
 
+  if (!event) {
+    return <Text>Event not found</Text>;
+  }
+
   return (
-    <View>
-      <Text>EventDetails: {event?.title}</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{ title: event?.title, headerBackTitleVisible: false, headerTintColor: '#333' }}
+      />
+      <View className="flex-1 gap-3 bg-white p-4">
+        <Image className="aspect-video w-full rounded-xl" source={{ uri: event.image }} />
+        <Text className="text-3xl font-bold" numberOfLines={2}>
+          {event.title}
+        </Text>
+        <Text className="text-md font-semibold uppercase text-amber-700">
+          {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
+        </Text>
+        <Text className="text-md" numberOfLines={2}>
+          {event.description}
+        </Text>
+      </View>
+    </>
   );
 };
 
